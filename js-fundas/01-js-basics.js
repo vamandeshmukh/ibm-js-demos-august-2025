0ff86dbbfd2d52aa502d712a46376537a61192e1
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-09-02 14:25:26 +0530

    js basics

---
+++ js-fundas/01-js-basics.js
@@ -0,0 +1,21 @@
+// console.log("JavaScript Basics");
+
+// Array and object in JS
+// array 
+const friends = ["Sonu", "Monu", "Tonu", "Ponu", 10, 10.50, true];
+// console.log(friends);
+
+// for (friend in friends)
+//     console.log(friend);
+
+// for (friend of friends) {
+//     console.log(friend);
+// }
+
+// console.log(friends[2]);
+console.log(friends);
+friends[2] = "Tonuuuuuuu";
+console.log(friends);
+
+// obejct 
+// const employee = {};
--- js-fundas/script.js
+++ js-fundas/script.js
@@ -1,35 +1,35 @@
-// // // JavaScript Fundas 
-// // // console.log("Hello world!");
+// // // // JavaScript Fundas 
+// // // // console.log("Hello world!");
 
-// // // name1 = "Hello world!";
-// // // name2 = 'Hello world!';
-// // // name3 = `Hello world!`;
-// // // console.log(name1);
-// // // console.log(name1);
-// // // console.log(name1);
+// // // // name1 = "Hello world!";
+// // // // name2 = 'Hello world!';
+// // // // name3 = `Hello world!`;
+// // // // console.log(name1);
+// // // // console.log(name1);
+// // // // console.log(name1);
 
-// // firstName = "Sonu";
-// // lastName = "Singh";
-// // // fullName = firstName + " " + lastName;
-// // fullName = `${firstName} ${lastName}`;
-// // console.log(fullName);
+// // // firstName = "Sonu";
+// // // lastName = "Singh";
+// // // // fullName = firstName + " " + lastName;
+// // // fullName = `${firstName} ${lastName}`;
+// // // console.log(fullName);
 
-// // ==========================
-// // Variable declaration in JS 
-// // ==========================
+// // // ==========================
+// // // Variable declaration in JS 
+// // // ==========================
 
-// // num1 = 10; // don't use this 
-// // var num2 = 20; // don't use this also 
-// // const num3 = 30; // preferred choice 
-// // let num4 = 40; // this also 
+// // // num1 = 10; // don't use this 
+// // // var num2 = 20; // don't use this also 
+// // // const num3 = 30; // preferred choice 
+// // // let num4 = 40; // this also 
 
-// // Java 
-// // int num = 10;
+// // // Java 
+// // // int num = 10;
 
-// // const num = 10;
-// // console.log(num);
-// // num = 20;
-// // console.log(num);
+// // // const num = 10;
+// // // console.log(num);
+// // // num = 20;
+// // // console.log(num);
 
 // let num = 10;
 // console.log(num);
@@ -52,10 +52,13 @@
 // num = {};
 // console.log(num);
 // console.log(typeof num);
+// num = () => { };
+// // console.log(num);
+// console.log(typeof num);
 
-const num1 = 10
-console.log(num1)
-const num2 = 10;
-console.log(num2);
+// // const num1 = 10
+// // console.log(num1)
+// // const num2 = 10;
+// // console.log(num2);
 
-// What is ECMAAcript? 
+// // What is ECMAAcript? 
