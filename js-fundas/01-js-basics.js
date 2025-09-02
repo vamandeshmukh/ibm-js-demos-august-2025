eb28c1a08b8efd225d01ef31469db546644f1297
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-09-02 15:23:32 +0530

    js basics

--- js-fundas/01-js-basics.js
+++ js-fundas/01-js-basics.js
@@ -1,104 +1,106 @@
-// // // console.log("JavaScript Basics");
+// // // // console.log("JavaScript Basics");
 
-// // // Array and object in JS
-// // // array 
-// // const friends = ["Sonu", "Monu", "Tonu", "Ponu", 10, 10.50, true];
+// // // // Array and object in JS
+// // // // array 
+// // // const friends = ["Sonu", "Monu", "Tonu", "Ponu", 10, 10.50, true];
+// // // // console.log(friends);
+
+// // // // for (friend in friends)
+// // // //     console.log(friend);
+
+// // // // for (friend of friends) {
+// // // //     console.log(friend);
+// // // // }
+
+// // // // console.log(friends[2]);
+// // // console.log(friends);
+// // // friends[2] = "Tonuuuuuuu";
 // // // console.log(friends);
 
-// // // for (friend in friends)
-// // //     console.log(friend);
+// // // // obejct 
+// // // const employee = {
+// // //     id: 101, name: "Sonu Reddy", salary: 10.50, phones: [9876543210, 6789012345],
+// // //     address: { street: "Bank street", city: "Hyderabad", pin: 500001 }, isIndian: true,
+// // //     printName: () => { console.log(employee.jobTitle); },
+// // //     updateData: (data) => { employee.jobTitle = data },
+// // //     jobTitle: "Sr Manager",
+// // //     sample: class {
+// // //         id;
+// // //         name;
+// // //         salary;
+// // //         constructor() {}
+// // //         getSalary() {}
+// // //     }
+// // // };
 
-// // // for (friend of friends) {
-// // //     console.log(friend);
-// // // }
+// // // console.log(employee);
+// // // console.log(employee.address.city);
+// // // employee.printName();
+// // // employee.updateData("VP");
+// // // employee.printName();
 
-// // // console.log(friends[2]);
-// // console.log(friends);
-// // friends[2] = "Tonuuuuuuu";
-// // console.log(friends);
+// // // function fun () {
+// // //     console.log(this);
+// // // };
+// // // console.log(this);
 
-// // // obejct 
-// // const employee = {
-// //     id: 101, name: "Sonu Reddy", salary: 10.50, phones: [9876543210, 6789012345],
-// //     address: { street: "Bank street", city: "Hyderabad", pin: 500001 }, isIndian: true,
-// //     printName: () => { console.log(employee.jobTitle); },
-// //     updateData: (data) => { employee.jobTitle = data },
-// //     jobTitle: "Sr Manager",
-// //     sample: class {
-// //         id;
-// //         name;
-// //         salary;
-// //         constructor() {}
-// //         getSalary() {}
-// //     }
-// // };
+// // // fun();
 
-// // console.log(employee);
-// // console.log(employee.address.city);
-// // employee.printName();
-// // employee.updateData("VP");
-// // employee.printName();
+// // // functions in JS 
 
-// // function fun () {
-// //     console.log(this);
-// // };
-// // console.log(this);
+// // // Old JS function 
 
-// // fun();
+// // // function fun() {
+// // //     console.log("fun function invoked.");
+// // // };
 
-// // functions in JS 
+// // // fun();
 
-// // Old JS function 
+// // // New JS function 
 
-// // function fun() {
-// //     console.log("fun function invoked.");
-// // };
+// // // // const fun = () => { console.log("fun function invoked."); };
+// // // const fun = () => console.log("fun function invoked.");
+// // // fun();
 
-// // fun();
+// // // const getGstAmount1 = (amount) => { return amount * 0.18; };
+// // // const getGstAmount2 = (amount) => amount * 0.18;
+// // // const getGstAmount3 = amount => amount * 0.18;
 
-// // New JS function 
+// // // const addNums = (a, b) => { return a + b };
+// // const addNums = (a, b) => a + b;
 
-// // // const fun = () => { console.log("fun function invoked."); };
-// // const fun = () => console.log("fun function invoked.");
-// // fun();
+// // let sum1 = addNums();
+// // let sum2 = addNums(10);
+// // let sum3 = addNums(10, 20);
+// // let sum4 = addNums(10, 20, 30);
+// // let sum5 = addNums("abc", "def");
+// // console.log(sum1);
+// // console.log(sum2);
+// // console.log(sum3);
+// // console.log(sum4);
+// // console.log(sum5);
 
-// // const getGstAmount1 = (amount) => { return amount * 0.18; };
-// // const getGstAmount2 = (amount) => amount * 0.18;
-// // const getGstAmount3 = amount => amount * 0.18;
+// // let num;
+// // console.log(num);
+// // console.log(typeof num);
 
-// // const addNums = (a, b) => { return a + b };
-// const addNums = (a, b) => a + b;
 
-// let sum1 = addNums();
-// let sum2 = addNums(10);
-// let sum3 = addNums(10, 20);
-// let sum4 = addNums(10, 20, 30);
-// let sum5 = addNums("abc", "def");
+// const subNums = (a, b) => {
+//     console.log(a); // "10"
+//     console.log(b); // 5
+//     return a + b;
+// };
+
+// let sum1 = subNums();
 // console.log(sum1);
-// console.log(sum2);
-// console.log(sum3);
-// console.log(sum4);
-// console.log(sum5);
-
-// let num;
-// console.log(num);
-// console.log(typeof num);
+// sum1 = subNums(10);
+// console.log(sum1);
+// sum1 = subNums(10, 5);
+// console.log(sum1);
+// sum1 = subNums(10, true);
+// console.log(sum1);
 
 
-const subNums = (a, b) => {
-    console.log(a); // "10"
-    console.log(b); // 5
-    return a + b;
-};
+const fun = () => console.log("fun function invoked.");
 
-let sum1 = subNums();
-console.log(sum1);
-sum1 = subNums(10);
-console.log(sum1);
-sum1 = subNums(10, 5);
-console.log(sum1);
-sum1 = subNums(10, true);
-console.log(sum1);
-
-
-
+console.log(fun());
