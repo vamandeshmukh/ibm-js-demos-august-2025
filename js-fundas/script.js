65b22a110bdafb00c2a5222644826d89c2426adb
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-09-02 13:01:57 +0530

    initial commit

--- js-fundas/script.js
+++ js-fundas/script.js
@@ -1,1 +1,61 @@
-console.log("Hello world!");
+// // // JavaScript Fundas 
+// // // console.log("Hello world!");
+
+// // // name1 = "Hello world!";
+// // // name2 = 'Hello world!';
+// // // name3 = `Hello world!`;
+// // // console.log(name1);
+// // // console.log(name1);
+// // // console.log(name1);
+
+// // firstName = "Sonu";
+// // lastName = "Singh";
+// // // fullName = firstName + " " + lastName;
+// // fullName = `${firstName} ${lastName}`;
+// // console.log(fullName);
+
+// // ==========================
+// // Variable declaration in JS 
+// // ==========================
+
+// // num1 = 10; // don't use this 
+// // var num2 = 20; // don't use this also 
+// // const num3 = 30; // preferred choice 
+// // let num4 = 40; // this also 
+
+// // Java 
+// // int num = 10;
+
+// // const num = 10;
+// // console.log(num);
+// // num = 20;
+// // console.log(num);
+
+// let num = 10;
+// console.log(num);
+// console.log(typeof num);
+// num = 20;
+// console.log(num);
+// console.log(typeof num);
+// num = 12.50;
+// console.log(num);
+// console.log(typeof num);
+// num = "abc";
+// console.log(num);
+// console.log(typeof num);
+// num = false;
+// console.log(num);
+// console.log(typeof num);
+// num = [];
+// console.log(num);
+// console.log(typeof num);
+// num = {};
+// console.log(num);
+// console.log(typeof num);
+
+const num1 = 10
+console.log(num1)
+const num2 = 10;
+console.log(num2);
+
+// What is ECMAAcript? 
