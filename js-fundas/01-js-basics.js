bd7a801c6f0310a7f5ae2dca67549ad2da8a08b1
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-09-02 14:48:46 +0530

    js basics

--- js-fundas/01-js-basics.js
+++ js-fundas/01-js-basics.js
@@ -1,21 +1,71 @@
-// console.log("JavaScript Basics");
+// // console.log("JavaScript Basics");
 
-// Array and object in JS
-// array 
-const friends = ["Sonu", "Monu", "Tonu", "Ponu", 10, 10.50, true];
+// // Array and object in JS
+// // array 
+// const friends = ["Sonu", "Monu", "Tonu", "Ponu", 10, 10.50, true];
+// // console.log(friends);
+
+// // for (friend in friends)
+// //     console.log(friend);
+
+// // for (friend of friends) {
+// //     console.log(friend);
+// // }
+
+// // console.log(friends[2]);
+// console.log(friends);
+// friends[2] = "Tonuuuuuuu";
 // console.log(friends);
 
-// for (friend in friends)
-//     console.log(friend);
+// // obejct 
+// const employee = {
+//     id: 101, name: "Sonu Reddy", salary: 10.50, phones: [9876543210, 6789012345],
+//     address: { street: "Bank street", city: "Hyderabad", pin: 500001 }, isIndian: true,
+//     printName: () => { console.log(employee.jobTitle); },
+//     updateData: (data) => { employee.jobTitle = data },
+//     jobTitle: "Sr Manager",
+//     sample: class {
+//         id;
+//         name;
+//         salary;
+//         constructor() {}
+//         getSalary() {}
+//     }
+// };
 
-// for (friend of friends) {
-//     console.log(friend);
-// }
+// console.log(employee);
+// console.log(employee.address.city);
+// employee.printName();
+// employee.updateData("VP");
+// employee.printName();
 
-// console.log(friends[2]);
-console.log(friends);
-friends[2] = "Tonuuuuuuu";
-console.log(friends);
+// function fun () {
+//     console.log(this);
+// };
+// console.log(this);
 
-// obejct 
-// const employee = {};
+// fun();
+
+// functions in JS 
+
+// Old JS function 
+
+// function fun() {
+//     console.log("fun function invoked.");
+// };
+
+// fun();
+
+// New JS function 
+
+// // const fun = () => { console.log("fun function invoked."); };
+// const fun = () => console.log("fun function invoked.");
+// fun();
+
+// const addNums = (a, b) => { return a + b };
+// const addNums = (a, b) => a + b;
+
+const getGstAmount1 = (amount) => { return amount * 0.18; };
+const getGstAmount2 = (amount) => amount * 0.18;
+const getGstAmount3 = amount => amount * 0.18;
+
