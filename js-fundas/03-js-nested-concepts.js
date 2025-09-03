0d925603a0a34289c628eb23ffa695cc749ec4b0
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-09-03 11:56:44 +0530

    concepts js

--- js-fundas/03-js-nested-concepts.js
+++ js-fundas/03-js-nested-concepts.js
@@ -1,13 +1,23 @@
 // console.log("03-js-nested-concepts.js");
 
+// const employee = {
+//     id: 101, name: "Sonu", address: { city: "Bengaluru", pin: 400454 }
+// };
+
+// // const { id, name, address: { city, pin } } = { ...employee };
+// const { id: eid, name: ename, address: { city, pin } } = { ...employee };
+
+// // console.log(id); // erro  
+// // console.log(address); // error 
+// console.log(eid); // 101
+// console.log(city); // Bengaluru
+
 const employee = {
-    id: 101, name: "Sonu", address: { city: "Bengaluru", pin: 400454 }
+    id: 101, name: "Sonu", salary: 10.50
 };
 
-// const { id, name, address: { city, pin } } = { ...employee };
-const { id: eid, name: ename, address: { city, pin } } = { ...employee };
+const { id, name, salary, department } = { ...employee };
 
-// console.log(id); // erro  
-// console.log(address); // error 
-console.log(eid); // 101
-console.log(city); // Bengaluru
+console.log(id); 
+console.log(department); // 
+
