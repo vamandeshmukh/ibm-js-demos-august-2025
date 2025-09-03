ce370f26d5216c5160054201c1d32136ae7d9a17
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-09-03 12:28:41 +0530

    concepts js

---
+++ js-fundas/04-js-build-in-functions.js
@@ -0,0 +1,18 @@
+// console.log("04-js-build-in-functions");
+
+// =====================
+// Array functions in JS 
+// =====================
+
+
+
+
+// ======================
+// Object functions in JS 
+// ======================
+
+
+// =========================
+// Functtion functions in JS 
+// =========================
+
