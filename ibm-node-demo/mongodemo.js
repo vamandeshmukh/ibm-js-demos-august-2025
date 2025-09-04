7bd2405e21710b5038f2b7ff10aab7762e34a270
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-09-04 11:57:42 +0530

    oop

--- ibm-node-demo/mongodemo.js
+++ ibm-node-demo/mongodemo.js
@@ -3,7 +3,26 @@ console.log("mongodemo.js");
 import mongoose from 'mongoose';
 
 export const dbcon = () => {
+    const Employee = mongoose.model("Employee", { name: String, phone: String, email: String, });
 
     mongoose.connect('mongodb://localhost:27017/ibm-demo')
-        .then(() => console.log('Connected!'));
-}
+        .then(() => Employee.find())
+        .then(employees => console.log(employees))
+        .catch(error => console.error(error))
+        .finally(() => mongoose.connection.close());
+};
+
+
+// export const dbcon = async () => {
+//     const Employee = mongoose.model("Employee", { name: String, phone: String, email: String, });
+//     try {
+//         await mongoose.connect('mongodb://localhost:27017/ibm-demo');
+//         const employees = await Employee.find();
+//         console.log(employees);
+//     } catch (error) {
+//         console.error(error);
+//     } finally {
+//         await mongoose.connection.close();
+//     }
+// };
+
