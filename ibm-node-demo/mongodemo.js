e6149da1fb17895f5b294a27750a0f0ce32270cc
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-09-04 12:45:51 +0530

    node project

--- ibm-node-demo/mongodemo.js
+++ ibm-node-demo/mongodemo.js
@@ -2,30 +2,30 @@ console.log("mongodemo.js");
 
 import mongoose from 'mongoose';
 
-export const dbcon = () => {
+// export const dbcon = () => {
+
+//     const Employee = mongoose.model("Employee", { name: String, email: String, });
+
+//     mongoose.connect('mongodb://localhost:27017/ibm-demo')
+//         .then(() => Employee.find())
+//         .then(employees => console.log(employees))
+//         .catch(error => console.error(error))
+//         .finally(() => mongoose.connection.close());
+// };
+
+
+export const dbcon = async () => {
 
     const Employee = mongoose.model("Employee", { name: String, phone: String, email: String, });
 
-    mongoose.connect('mongodb://localhost:27017/ibm-demo')
-        .then(() => Employee.find())
-        .then(employees => console.log(employees))
-        .catch(error => console.error(error))
-        .finally(() => mongoose.connection.close());
+    try {
+        await mongoose.connect('mongodb://localhost:27017/ibm-demo');
+        const employees = await Employee.find();
+        console.log(employees);
+    } catch (error) {
+        console.error(error);
+    } finally {
+        await mongoose.connection.close();
+    }
 };
-
-
-// export const dbcon = async () => {
-
-//     const Employee = mongoose.model("Employee", { name: String, phone: String, email: String, });
-
-//     try {
-//         await mongoose.connect('mongodb://localhost:27017/ibm-demo');
-//         const employees = await Employee.find();
-//         console.log(employees);
-//     } catch (error) {
-//         console.error(error);
-//     } finally {
-//         await mongoose.connection.close();
-//     }
-// };
 
