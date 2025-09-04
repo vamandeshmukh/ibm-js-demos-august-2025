daf363de0c5e62a9d69076b967faa71f33482032
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-09-04 11:00:40 +0530

    oop

--- ibm-node-demo/calc.js
+++ ibm-node-demo/calc.js
@@ -1,5 +1,17 @@
 console.log("calc.js");
 
-export const addNums = (a, b) => a + b;
+// export const addNums = (a, b) => a + b;
+// export const subNums = (a, b) => a - b;
 
-export const subNums = (a, b) => a - b;
+// const addNums = (a, b) => a + b;
+// const subNums = (a, b) => a - b;
+// export { addNums, subNums };
+
+
+export const calc = {
+    addNums: (a, b) => a + b,
+    subNums: (a, b) => a - b
+}
+
+
+// export { addNums, subNums };
--- ibm-node-demo/script.js
+++ ibm-node-demo/script.js
@@ -1,7 +1,12 @@
 console.log("script.js");
 
-import { addNums, subNums } from "./calc.js";
+// import { addNums, subNums } from "./calc.js";
+// const sum = addNums(10, 20);
+// console.log(sum);
+// console.log(subNums(10, 5));
 
-const sum = addNums(10, 20);
+
+import { calc } from "./calc.js";
+const sum = calc.addNums(10, 20);
 console.log(sum);
-console.log(subNums(10, 5));
+console.log(calc.subNums(10, 5));
